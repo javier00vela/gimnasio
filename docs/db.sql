f56ace315063af00292bc8952dd6241c99dc29ff
javier <javier00vela@gmail.com>
2021-02-09 13:27:08 -0500

    se agrego la sentencia de create database en el archivo db

--- docs/db.sql
+++ docs/db.sql
@@ -1,96 +1,187 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2021 at 07:25 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 100417
- Source Host           : localhost:3306
- Source Schema         : gimnasio
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100417
- File Encoding         : 65001
 
- Date: 28/01/2021 11:19:53
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `gimnasio`
+--
+CREATE DATABASE IF NOT EXISTS `gimnasio` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gimnasio`;
 
--- ----------------------------
--- Table structure for ciudad
--- ----------------------------
-DROP TABLE IF EXISTS `ciudad`;
-CREATE TABLE `ciudad`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of ciudad
--- ----------------------------
-INSERT INTO `ciudad` VALUES (1, 'Bogotá d,c');
-INSERT INTO `ciudad` VALUES (2, 'Cali');
-INSERT INTO `ciudad` VALUES (6, 'cucuta');
+--
+-- Table structure for table `ciudad`
+--
 
--- ----------------------------
--- Table structure for perfil
--- ----------------------------
-DROP TABLE IF EXISTS `perfil`;
-CREATE TABLE `perfil`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `ciudad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Records of perfil
--- ----------------------------
-INSERT INTO `perfil` VALUES (1, 'administrador');
-INSERT INTO `perfil` VALUES (2, 'usuario');
+--
+-- Dumping data for table `ciudad`
+--
 
--- ----------------------------
--- Table structure for sede
--- ----------------------------
-DROP TABLE IF EXISTS `sede`;
-CREATE TABLE `sede`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_ciudad` int NOT NULL,
-  `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_ciudad`(`id_ciudad`) USING BTREE,
-  CONSTRAINT `fk_ciudad` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+INSERT INTO `ciudad` (`id`, `nombre`) VALUES
+(1, 'Bogotá d,c'),
+(2, 'Cali'),
+(6, 'cucuta');
 
--- ----------------------------
--- Records of sede
--- ----------------------------
-INSERT INTO `sede` VALUES (1, 1, 'sede 1 mayo - cll 20 a 25');
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for usuario
--- ----------------------------
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE `usuario`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_perfil` int NOT NULL,
-  `id_sede` int NULL DEFAULT NULL,
-  `nombres` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `apellidos` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `identificacion` int NOT NULL,
-  `contrasena` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_perfil`(`id_perfil`) USING BTREE,
-  INDEX `fk_sede`(`id_sede`) USING BTREE,
-  CONSTRAINT `fk_perfil` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_sede` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+--
+-- Table structure for table `perfil`
+--
 
--- ----------------------------
--- Records of usuario
--- ----------------------------
-INSERT INTO `usuario` VALUES (1, 1, 1, 'javier', 'vela', 12345, 'MTIzNDU=');
-INSERT INTO `usuario` VALUES (3, 1, 1, 'pepito', 'perez', 10001, 'MTIzNDU=');
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Dumping data for table `perfil`
+--
+
+INSERT INTO `perfil` (`id`, `nombre`) VALUES
+(1, 'administrador'),
+(2, 'usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sede`
+--
+
+CREATE TABLE `sede` (
+  `id` int(11) NOT NULL,
+  `id_ciudad` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `sede`
+--
+
+INSERT INTO `sede` (`id`, `id_ciudad`, `nombre`) VALUES
+(1, 1, 'sede 1 mayo - cll 20 a 25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `id_perfil` int(11) NOT NULL,
+  `id_sede` int(11) DEFAULT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `identificacion` int(11) NOT NULL,
+  `contrasena` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `id_perfil`, `id_sede`, `nombres`, `apellidos`, `identificacion`, `contrasena`) VALUES
+(1, 1, 1, 'javier', 'vela', 12345, 'MTIzNDU='),
+(3, 1, 1, 'pepito', 'perez', 10001, 'MTIzNDU=');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `sede`
+--
+ALTER TABLE `sede`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `fk_ciudad` (`id_ciudad`) USING BTREE;
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `fk_perfil` (`id_perfil`) USING BTREE,
+  ADD KEY `fk_sede` (`id_sede`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ciudad`
+--
+ALTER TABLE `ciudad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sede`
+--
+ALTER TABLE `sede`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sede`
+--
+ALTER TABLE `sede`
+  ADD CONSTRAINT `fk_ciudad` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`id`);
+
+--
+-- Constraints for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_perfil` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id`),
+  ADD CONSTRAINT `fk_sede` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
